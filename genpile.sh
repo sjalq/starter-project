03ce0575781694f98951e48ce3f2368f47ea1247
sjalq <schalk.dormehl@gmail.com>
2025-03-29 11:30:16 +0200

    added genpile

---
+++ genpile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Run elm-pages
+npx elm-pages run fusion/scripts/src/Main.elm elm.json generated Types.BackendModel
+
+# Compile lamdera files
+lamdera make src/Backend.elm src/Frontend.elm src/RPC.elm
+
+echo "Build completed successfully!"

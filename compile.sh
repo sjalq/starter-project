e2e65aea0dc775a5a965c8ece1bda164d9aa1143
Schalk Dormehl <schalk.dormehl@gmail.com>
2025-05-09 00:16:07 +0200

    Update .cursorignore to include additional directories and modify compile.sh to comment out elm-pages run command. Enhance lamdera.mdc with new guidelines on code removal, imports, and additional best practices.

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run elm-pages
-npx elm-pages run fusion/scripts/src/Main.elm elm.json generated Types.BackendModel
+# npx elm-pages run fusion/scripts/src/Main.elm elm.json generated Types.BackendModel
 
 # Compile lamdera files
 lamdera make src/Backend.elm src/Frontend.elm src/RPC.elm
